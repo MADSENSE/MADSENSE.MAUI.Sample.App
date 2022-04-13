2aea28d1d73719f5c5fd31dfa559578b988d9828
Yann Alex Zahringer Ferrando <yann.zahringer@madsense.com>
2022-04-13 10:20:13 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MADSENSE.MAUI.Sample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MADSENSE.MAUI.Sample;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
